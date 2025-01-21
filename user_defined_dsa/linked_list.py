9264f19212147750d2ef706dd381bb38eb31e306
humphreyokoth <humpasp@gmail.com>
2025-01-21 13:34:37 +0300

    Add detailed documentation for singly linked list operations and characteristics

--- user_defined_dsa/linked_list.py
+++ user_defined_dsa/linked_list.py
@@ -27,3 +27,57 @@
 # 1. Singly linked list
 # 2. Doubly linked list
 # 3. Circular linked list
+
+
+# singly linked list
+# is a chain of data structures where each data structure contains data and a pointer to the next data structure in the chain
+# head is the first node of the linked list
+# tail is the last node of the linked list which points to null
+# singly linked list is a linear data structure
+# singly linked list is a dynamic data structure
+# singly linked list is not stored in contiguous memory locations
+# singly linked list is not indexed
+# singly linked list is not ordered
+# singly linked list is not mutable
+# singly linked list is not nested
+# singly linked list is not nested list
+# singly linked list is not nested tuple
+# singly linked list is not nested dictionary
+# singly linked list is not nested set
+
+# Operations 
+# 1. Insertion
+# 2. Deletion
+# 3. Traversing
+
+
+# Adding elements to the linked list 
+# 1. At the beginning of the linked list
+# 2. At the end of the linked list
+# 3. At the middle or inbetween of the linked list
+
+# Adding elements to the linked list
+# 1.Create a node
+# 2.Change new node next to head
+# 3.Point head to the new node
+
+# Add element to the end of a linked list 
+# 1. Create a node
+# 2. Go to the last node
+# 3. Change the next of the last node to the new node
+
+# Add node inbetween two nodes
+# 1. Create a node
+# 2. Go to the previous node
+
+# Deleting elements from the linked list
+# 1. At the beginning of the linked list
+# 2. At the end of the linked list
+# 3. At the middle or inbetween of the linked list
+
+
+
+# Traverse the linked list
+
+# 1. Start from the head node
+# 2. Traverse the linked list until we reach the last node
