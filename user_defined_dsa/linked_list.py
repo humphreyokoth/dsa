a278502ebcb9f58b01f2376fb0ec636e93f0c059
humphreyokoth <humpasp@gmail.com>
2025-01-21 13:07:50 +0300

    Add linked list implementation with detailed explanations and advantages/disadvantages

---
+++ user_defined_dsa/linked_list.py
@@ -0,0 +1,29 @@
+# linked list is chain of node in which each node contains data and a pointer to the next node
+# Nodes are connected using pointers
+# head /front is the first node of the linked list
+# tail/end is the last node of the linked list which points to null
+
+# its a dynamic data structure
+# it is not stored in contiguous memory location. It is stored in random memory locations
+
+# Advantages of linked list 
+# 1. Dynamic data structure
+# 2. Memory utilization is efficient
+# 3. Insertion and deletion of elements are easy
+# 4. Size of the linked list can be changed dynamically
+# 5. Implementation is easy
+# 6. No need to specify the size of the linked list
+# 7.can be used to implement stack queue and graph
+# 8.Respresent and manipulate polynomials.  Polynomial can be represented as a linked list where each node contains the coefficient and the power of the variable in the polynomial term.
+
+# Disadvantages of linked list
+# 1. Random access is not allowed. We have to access the elements sequentially starting from the head node.
+# 2. Extra memory is required for the pointer to the next node.
+# 3. Reverse traversing is difficult in a singly linked list.
+# 4. Memory utilization is inefficient as pointers require extra memory.
+
+
+# Types of linked list
+# 1. Singly linked list
+# 2. Doubly linked list
+# 3. Circular linked list
