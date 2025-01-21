82752190ea5f39b2e4340f3265a8da18171d02b0
humphreyokoth <humpasp@gmail.com>
2025-01-21 11:21:58 +0300

    Add stack implementation with size limit and interactive push/pop operations

--- user_defined_dsa/stacks.py
+++ user_defined_dsa/stacks.py
@@ -79,6 +79,7 @@
 # stack.append(50)
 # print(stack)
 
+# stack.pop()
 stack  = []
 def push():
     element = input("Enter the element: ")
@@ -104,4 +105,39 @@ while True:
         break
     else:
         print("Enter the correct operation")
+
+
+# Stack with limit
+
+stack = []
+def push():
+    if len(stack) == n:
+        print("Stack is full")
+    else:
+        element = input("Enter the element: ")
+        stack.append(element)
+        print(stack)
+
+def pop():
+    if not stack:
+        print("Stack is empty")
+    else:
+        e = stack.pop()
+        print("Removed element: ", e)
+        print(stack)
+
+n = int(input("Enter the size of the stack: "))
+
+while True:
+    print("Select the operation 1. push 2. pop 3. quit")
+    choice = int(input())
+    if choice ==1:
+        push()
+    elif choice ==2:
+        pop()
+    elif choice ==3:
+        break
+    else:
+        print("Enter the correct operation")
+
 
