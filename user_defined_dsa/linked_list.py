00b6bb5c693cd8dffac59064bd885a73fc963a05
humphreyokoth <humpasp@gmail.com>
2025-01-22 13:51:48 +0300

    Implement add_begin method to insert elements at the beginning of the linked list

--- user_defined_dsa/linked_list.py
+++ user_defined_dsa/linked_list.py
@@ -112,3 +112,33 @@ class LinkedList:
 
 LL1 = LinkedList()
 LL1.print_LL()
+
+# Adding elements at the beginning of the linked list
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+    def add_begin(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+
+LL1 = LinkedList()
+LL1.add_begin(10)
+LL1.add_begin(20)
+LL1.print_LL()
