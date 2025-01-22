92db176332c844796a5d3c29856825a2a12d2772
humphreyokoth <humpasp@gmail.com>
2025-01-22 14:45:06 +0300

    Add add_end method to append elements to the end of the linked list and improve print_LL output

--- user_defined_dsa/linked_list.py
+++ user_defined_dsa/linked_list.py
@@ -127,18 +127,35 @@ class LinkedList:
 
     def print_LL(self):
         if self.head is None:
-            print("Linked list is empty")
+            print("Linked list is   empty")
         else:
             n = self.head
             while n is not None:
-                print(n.data)
+                print(n.data,"---->" ,end =  "")
                 n = n.ref
     def add_begin(self, data):
         new_node = Node(data)
         new_node.ref = self.head
         self.head = new_node
 
+# Add element at the end of the linked list
+    def add_end(self,data):
+        new_node  = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+
+
+
+
 LL1 = LinkedList()
 LL1.add_begin(10)
+LL1.add_end(100)
+LL1.add_end(200)
 LL1.add_begin(20)
 LL1.print_LL()
+
