9123b40e7636bb51f5503bcc5ebc6c8b725955bf
humphreyokoth <humpasp@gmail.com>
2025-02-01 06:39:28 +0300

    Add delete_end method to remove the last node in the linked list

--- user_defined_dsa/linked_list.py
+++ user_defined_dsa/linked_list.py
@@ -198,9 +198,23 @@ class LinkedList:
 # delete the first node in the linked list 
     def  delete_begin(self):
         if self.head is None:
-            print("Linked list is empty.add nodes")
+            print("Linked list is empty cant delete nodes")
         else:
             self.head = self.head.ref
+
+# delete the last node in the linked list
+
+    def delete_end(self):
+        if self.head is None:
+            print("Linked list is empty")
+        elif self.head.ref is None:
+            self.head = None
+        else:
+            n = self.head
+            while n.ref.ref is not None:
+                n = n.ref
+            n.ref = None
+
         
 
 
