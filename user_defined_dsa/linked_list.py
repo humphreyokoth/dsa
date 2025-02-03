2dcaaba152e4d39b520a1f276d01dec039192f30
humphreyokoth <humpasp@gmail.com>
2025-02-03 08:37:30 +0300

    Add delete_by_value method to remove a node by its value in the linked list

--- user_defined_dsa/linked_list.py
+++ user_defined_dsa/linked_list.py
@@ -215,7 +215,22 @@ class LinkedList:
                 n = n.ref
             n.ref = None
 
-        
+    def delete_by_value(self, x):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        if x == self.head.data:
+            self.head = self.head.ref
+            return
+        n = self.head
+        while n.ref is not None:
+            if x == n.ref.data:
+                break
+            n = n.ref
+        if n.ref is None:
+            print("Node is not present in the linked list")
+        else:
+            n.ref = n.ref.ref
 
 
 
