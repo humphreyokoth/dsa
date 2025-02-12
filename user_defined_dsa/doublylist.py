4a2aeea67cf5cafea53bedeb398c9b1f78be9e7d
humphreyokoth <humpasp@gmail.com>
2025-02-12 11:55:38 +0300

    Add methods to insert elements at the beginning and end of the doubly linked list

--- user_defined_dsa/doublylist.py
+++ user_defined_dsa/doublylist.py
@@ -39,6 +39,29 @@ class doublyLL:
         else:
             print("Linked list is not empty")
 
+# Add element at the beginning of the  doubly linked list
+
+    def add_begin(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            new_node.nref = self.head
+            self.head.pref = new_node
+            self.head = new_node
+
+# add elemen at the end of the doubly linked list
+
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.nref = new_node
+
 dl1 =   doublyLL()
 dl1.print_LL()
 dl1.print_LL_reverse()
